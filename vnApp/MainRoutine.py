866141f366b372402e9ae27ad2c2bda547d15106
Andy Shao <hui.shao@xor-media.tv>
2018-09-06 17:38:17 +0800

    runAsDaemon

--- vnApp/MainRoutine.py
+++ vnApp/MainRoutine.py
@@ -60,8 +60,9 @@ class BaseApplication(object):
         """Constructor"""
         self._engine = mainRoutine
         self._settings = settings
-
         self._active = False                     # 工作状态
+
+        self._pid = os.getpid() # process id
 
         # the app instance Id
         self._id = settings.id("")
@@ -502,6 +503,47 @@ class MainRoutine(object):
             #     self.debug(u'MainThread heartbeat')
 
         self.info(u'MainRoutine finish looping')
+
+    def runAsDaemon(self, stdin='/dev/null',stdout='/dev/null',stderr='/dev/null'):
+        import sys
+        sys.stdin  = open(stdin,'r')
+        sys.stdout = open(stdout,'a+')
+        sys.stderr = open(stderr,'a+')
+        
+        try:
+            self._pid = os.fork()
+            if self._pid > 0:        #parrent
+                os._exit(0)
+        except OSError,e:
+            sys.stderr.write("first fork failed!!"+e.strerror)
+            os._exit(1)
+    
+        ''' 子进程， 由于父进程已经退出，所以子进程变为孤儿进程，由init收养
+        setsid使子进程成为新的会话首进程，和进程组的组长，与原来的进程组、控制终端和登录会话脱离。
+        '''
+        os.setsid()
+
+        '''防止在类似于临时挂载的文件系统下运行，例如/mnt文件夹下，这样守护进程一旦运行，临时挂载的文件系统就无法卸载了，这里我们推荐把当前工作目录切换到根目录下'''
+        os.chdir("/")
+        '''设置用户创建文件的默认权限，设置的是权限“补码”，这里将文件权限掩码设为0，使得用户创建的文件具有最大的权限。否则，默认权限是从父进程继承得来的'''
+        os.umask(0)
+    
+        try:
+            self._pid = os.fork()     #第二次进行fork,为了防止会话首进程意外获得控制终端
+            if self._pid > 0:
+                os._exit(0)     #父进程退出
+        except OSError,e:
+            sys.stderr.write("second fork failed!!"+e.strerror)
+            os._exit(1)
+    
+        # 孙进程
+        #   for i in range(3,64):  # 关闭所有可能打开的不需要的文件，UNP中这样处理，但是发现在python中实现不需要。
+        #       os.close(i)
+        sys.stdout.write("Daemon has been created! with self._pid: %d\n" % os.getpid())
+        sys.stdout.flush()  #由于这里我们使用的是标准IO，回顾APUE第五章，这里应该是行缓冲或全缓冲，因此要调用flush，从内存中刷入日志文件。\
+
+        self.loop()
+    
 
     #----------------------------------------------------------------------
     def subscribeMarketData(self, subscribeReq, dsName):

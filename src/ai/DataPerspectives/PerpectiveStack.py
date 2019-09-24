ad33eb0229d4d6fcfd117ebb870dd0f6fb104c57
Andy Shao <hui.shao@xor-media.tv>
2019-09-24 13:35:03 +0800

    take MarketData.EVENT_ to index the Perspective parts

--- src/ai/DataPerspectives/PerpectiveStack.py
+++ src/ai/DataPerspectives/PerpectiveStack.py
@@ -1,22 +1,41 @@
+# encoding: UTF-8
+
+from __future__ import division
+
+from marketdata.mdBackEnd import MarketData
+
+from datetime import datetime
+from abc import ABCMeta, abstractmethod
+
 ########################################################################
 class MDPerspective(object):
     '''
-    Data structure of Perspective：
+    Data structure of Perspective:
     1. Ticks
     2. 1min KLines
     3. 5min KLines
     4. 1day KLines
     '''
+    # EVENT_TICK          = MARKETDATE_EVENT_PREFIX + 'Tick'                   # TICK行情事件，可后接具体的vtSymbol
+    # EVENT_MARKET_DEPTH0 = MARKETDATE_EVENT_PREFIX + 'MD0'           # Market depth0
+    # EVENT_MARKET_DEPTH2 = MARKETDATE_EVENT_PREFIX + 'MD2'           # Market depth2
+    # EVENT_KLINE_1MIN    = MARKETDATE_EVENT_PREFIX + 'KL1m'
+    # EVENT_KLINE_5MIN    = MARKETDATE_EVENT_PREFIX + 'KL5m'
+    # EVENT_KLINE_15MIN   = MARKETDATE_EVENT_PREFIX + 'KL15m'
+    # EVENT_KLINE_30MIN   = MARKETDATE_EVENT_PREFIX + 'KL30m'
+    # EVENT_KLINE_1HOUR   = MARKETDATE_EVENT_PREFIX + 'KL1h'
+    # EVENT_KLINE_4HOUR   = MARKETDATE_EVENT_PREFIX + 'KL4h'
+    # EVENT_KLINE_30MIN    = MARKETDATE_EVENT_PREFIX + 'KL1d'
 
     #----------------------------------------------------------------------
     def __init__(self, exchange, symbol =None):
         '''Constructor'''
         super(MDPerspective, self).__init__()
         self._data = {
-            ticks:   [],
-            KL_1min: [],
-            KL_5min: [],
-            KL_1day: [],
+            MarketData.EVENT_TICK:   [],
+            MarketData.EVENT_KLINE_1MIN: [],
+            MarketData.EVENT_KLINE_5MIN: [],
+            MarketData.EVENT_KLINE_1DAY: [],
         }
         
         self._stampAsOf = None
@@ -32,7 +51,7 @@ class MDPerspective(object):
 ########################################################################
 class PerspectiveStack(object):
     '''
-    Perspective合成器，支持：
+    Perspective合成器，支持:
     1. 基于x分钟K线输入（X可以是1、5、day	）
     2. 基于Tick输入
     '''
@@ -41,10 +60,10 @@ class PerspectiveStack(object):
     def __init__(self, exchange, symbol, KLDepth_1min=60, KLDepth_5min=240, KLDepth_1day=220, tickDepth=120) :
         '''Constructor'''
         self._depth ={
-            ticks:   tickDepth,
-            KL_1min: KLDepth_1min,
-            KL_5min: KLDepth_5min,
-            KL_1day: KLDepth_1day,
+            MarketData.EVENT_TICK:   tickDepth,
+            MarketData.EVENT_KLINE_1MIN: KLDepth_1min,
+            MarketData.EVENT_KLINE_5MIN: KLDepth_5min,
+            MarketData.EVENT_KLINE_1DAY: KLDepth_1day,
         }
 
         self._symbol   =symbol
@@ -53,20 +72,26 @@ class PerspectiveStack(object):
         self._mergerKline1To5m   = KlineToXminMerger(self.cbMergedKLine5min, xmin=5)
         self._mergerKline5mToDay = KlineToXminMerger(self.cbMergedKLineDay,  xmin=240)
         self._currentPersective =  MDPerspective(self._symbol, self._exchange)
-        for i in range(self._depth.ticks) ：
+        for i in range(self._depth[MarketData.EVENT_TICK]) :
             self._currentPersective._data.ticks = [TickData(self._symbol, self._exchange)] + self._currentPersective._data.ticks
-        for i in range(self._depth.KL_1min) ：
+        for i in range(self._depth[MarketData.EVENT_KLINE_1MIN]) :
             self._currentPersective._data.KL_1min = [KLineData(self._symbol, self._exchange)] + self._currentPersective._data.KL_1min
-        for i in range(self._depth.KL_5min) ：
+        for i in range(self._depth[MarketData.EVENT_KLINE_5MIN]) :
             self._currentPersective._data.KL_5min = [KLineData(self._symbol, self._exchange)] + self._currentPersective._data.KL_5min
-        for i in range(self._depth.KLDepth_1day) ：
+        for i in range(self._depth[MarketData.EVENT_KLINE_1DAY]) :
             self._currentPersective._data.KLDepth_1day = [KLineData(self._symbol, self._exchange)] + self._currentPersective._data.KLDepth_1day
 
-        self._stampStepped = self._stampNoticed = datetime.now()
+        self._stampStepped = {
+            MarketData.EVENT_TICK:   None,
+            MarketData.EVENT_KLINE_1MIN: None,
+            MarketData.EVENT_KLINE_5MIN: None,
+            MarketData.EVENT_KLINE_1DAY: None,
+        }
+        self._stampNoticed = datetime.now()
         
     #----------------------------------------------------------------------
-    def pushKLineXmin(self, kline, scaleXmin=1) :
-        self._pushKLine(self, kline, scaleXmin) :
+    def pushKLineXmin(self, kline, kltype =MarketData.EVENT_KLINE_1MIN) :
+        self._pushKLine(self, kline, kltype)
         if self._stampNoticed < self._stampStepped :
             self._currentPersective._stampAsOf = self._stampStepped
             self.OnNewPerspective(copy(self._currentPersective))
@@ -75,53 +100,69 @@ class PerspectiveStack(object):
     def OnNewPerspective(self, persective) :
         pass
            
+    def OnKLinePrefillWished(self, kltype) :
+        # dummy setting the stamp
+        self._stampStepped[kltype] = datetime.now()
+
     #----------------------------------------------------------------------
     # private callbacks for the mergers
     def cbMergedKLine1min(self, kline):
-        self._pushKLine(kline, scaleXmin=1)
+        self._pushKLine(kline, kltype =MarketData.EVENT_KLINE_1MIN)
 
     def cbMergedKLine5min(self, kline):
-        self._pushKLine(kline, scaleXmin=5)
+        self._pushKLine(kline, kltype =MarketData.EVENT_KLINE_5MIN)
 
     def cbMergedKLineDay(self, kline):
-        self._pushKLine(kline, scaleXmin=240)
+        self._pushKLine(kline, kltype =MarketData.EVENT_KLINE_1DAY)
 
     #----------------------------------------------------------------------
-    def _pushKLine(self, kline, scaleXmin=1):
+    def _pushKLine(self, kline, kltype =MarketData.EVENT_KLINE_1MIN):
         '''X分钟K线更新'''
 
         merger = None
-        if scaleXmin ==1:
-            klines = self._currentPersective.KL_1min
+        nextKLT = MarketData.EVENT_TICK
+        if MarketData.EVENT_KLINE_1MIN == kltype:
+            nextKLT = MarketData.EVENT_KLINE_5MIN
             merger = self._mergerKline1To5m
-        elif scaleXmin ==5:
-            klines = self._currentPersective.KL_5min
+        if MarketData.EVENT_KLINE_5MIN == kltype:
+            nextKLT = MarketData.EVENT_KLINE_1DAY
             merger = self._mergerKline5mToDay
-        elif scaleXmin ==240:
-            klines = self._currentPersective.KL_1day
+        if MarketData.EVENT_KLINE_5MIN == kltype:
+            nextKLT = MarketData.EVENT_TICK
 
-        if (kline.exchange.find('_k2x') or kline.exchange.find('_t2k')) and klines[0].datetime > kline.datetime:
-            return // ignore the late merging
+        klines = self._currentPersective._data.get(kltype)
+        if klines and len(klines) >0 :
+            if (kline.exchange.find('_k2x') or kline.exchange.find('_t2k')) and \
+                klines[0].datetime and kline.datetime < klines[0].datetime:
+                return # ignore the late merging
 
-        if klines[0].datetime.minute == kline.datetime.minute && klines[0].datetime.date == kline.datetime.date:
-            klines[0] = kline
-        else:
-            klines = [kline] + klines[1:] # shift the existing list and push new kline at the front
-            self._stampStepped = datetime.now()
+            if klines[0].datetime.minute == kline.datetime.minute and klines[0].datetime.date == kline.datetime.date :
+                klines[0] = kline # overwrite the frond Kline
+            else:
+                klines = [kline] + klines[1:] # shift the existing list and push new kline at the front
+                self._stampStepped[kltype] = kline.datetime
+
+        nextKL = None
+        if nextKLT != MarketData.EVENT_TICK :
+            nextKL = self._currentPersective._data.get(nextKLT)
+
+        if nextKL and not self._stampStepped[nextKLT] :
+            self.OnKLinePrefillWished(nextKLT)
 
         if merger:
             merger.pushKLine(kline)
         
     def pushTick(self, tick):
         '''Tick更新'''
-
-        merger = self._mergerTick21Min
-        ticks = self._currentPersective._data.ticks
-        if ticks[0].datetime and tick.datetime < ticks[0].datetime
+        ticks = self._currentPersective._data[MarketData.EVENT_TICK]
+        if ticks[0].datetime and tick.datetime < ticks[0].datetime :
             return
 
         ticks = [tick] + ticks[1:] # shift the existing list and push new tick at the front
-        self._stampStepped = datetime.now()
-        if merger:
-            merger.pushTick(tick)
+        self._stampStepped[MarketData.EVENT_TICK] = ticks.datetime
+        if not self._stampStepped[MarketData.EVENT_KLINE_1MIN] :
+            self.OnKLinePrefillWished(MarketData.EVENT_KLINE_1MIN)
+
+        if self._mergerTick21Min:
+            self._mergerTick21Min.pushTick(tick)
 

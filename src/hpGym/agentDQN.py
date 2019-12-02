283df1400c5fd4b0e9c549b727d24cb23b85955c
root <root@AHS-W10v11.localdomain>
2019-12-02 20:08:28 +0800

    fit() params comments

--- src/hpGym/agentDQN.py
+++ src/hpGym/agentDQN.py
@@ -68,6 +68,7 @@ class agentDQN(MetaAgent):
         '''
         if not self._gymTrader.dataRoot or not self._brain :
             raise ValueError("Null brain or Null trader")
+
         if not brainId or len(brainId) <=0:
             if self.__wkBrainId and len(brainId) >0:
                 brainId = brainId
@@ -152,10 +153,20 @@ class agentDQN(MetaAgent):
         q_target = self._brain.predict(state)
         q_target[action[0], action[1]] = reward
 
-        return self._brain.fit(state, q_target,
-                                batch_size=self._batchSize,
-                                epochs=1,
-                                verbose=False)
+        # x：输入数据。如果模型只有一个输入，那么x的类型是numpy array，如果模型有多个输入，那么x的类型应当为list，list的元素是对应于各个输入的numpy array
+        # y：标签，numpy array
+        # batch_size：整数，指定进行梯度下降时每个batch包含的样本数。训练时一个batch的样本会被计算一次梯度下降，使目标函数优化一步。
+        # epochs：整数，训练终止时的epoch值，训练将在达到该epoch值时停止，当没有设置initial_epoch时，它就是训练的总轮数，否则训练的总轮数为epochs - inital_epoch
+        # verbose：日志显示，0为不在标准输出流输出日志信息，1为输出进度条记录，2为每个epoch输出一行记录
+        # callbacks：list，其中的元素是keras.callbacks.Callback的对象。这个list中的回调函数将会在训练过程中的适当时机被调用，参考回调函数
+        # validation_split：0~1之间的浮点数，用来指定训练集的一定比例数据作为验证集。验证集将不参与训练，并在每个epoch结束后测试的模型的指标，如损失函数、精确度等。注意，validation_split的划分在shuffle之前，因此如果你的数据本身是有序的，需要先手工打乱再指定validation_split，否则可能会出现验证集样本不均匀。
+        # validation_data：形式为（X，y）的tuple，是指定的验证集。此参数将覆盖validation_spilt。
+        # shuffle：布尔值或字符串，一般为布尔值，表示是否在训练过程中随机打乱输入样本的顺序。若为字符串“batch”，则是用来处理HDF5数据的特殊情况，它将在batch内部将数据打乱。
+        # class_weight：字典，将不同的类别映射为不同的权值，该参数用来在训练过程中调整损失函数（只能用于训练）
+        # sample_weight：权值的numpy array，用于在训练时调整损失函数（仅用于训练）。可以传递一个1D的与样本等长的向量用于对样本进行1对1的加权，或者在面对时序数据时，传递一个的形式为（samples，sequence_length）的矩阵来为每个时间步上的样本赋不同的权。这种情况下请确定在编译模型时添加了sample_weight_mode=’temporal’。
+        # initial_epoch: 从该参数指定的epoch开始训练，在继续之前的训练时有用。
+        return self._brain.fit(x=state, y=q_target, epochs=1,
+                            batch_size=self._batchSize, verbose=False)
 
     def __get_batches(self):
         '''Selecting a batch of memory

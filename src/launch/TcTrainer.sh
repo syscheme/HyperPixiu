03445b80ab4eeb6935f1cff9ba25e92fe6c3654b
root <root@dazzle.localdomain>
2020-01-06 22:26:24 +0800

    tcTrainer.sh

---
+++ src/launch/TcTrainer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PROJLOC="/mnt/d/workspace.t3600/HyperPixiu"
+MODEL="VGG16d1.S1548I4A3"
+
+cd ${PROJLOC}
+if ! [ -d ./out/${MODEL} ]; then 
+    mkdir -p ./${MODEL} ; 
+fi
+
+PID=$(ps aux|grep 'DQNTrainer.py'|grep VGG16d1|awk '{print $2;}')
+if [ -z ${PID} ]; then
+     ./run.sh src/hpGym/DQNTrainer.py -f conf/DQNTrainer_VGG16d1.json &
+    PID=$(ps aux|grep 'DQNTrainer.py'|grep VGG16d1|awk '{print $2;}')
+    echo "started DQNTrainer with PID ${PID}"
+fi
+
+OUTDIR="./out/DQNTrainer_${PID}"
+
+if [ -e ${OUTDIR}/VGG16d1.S1548I4A3.weights.h5 ] ; then 
+    echo "packaging ${OUTDIR} into /tmp/${MODEL}_${STAMP}.tar.bz2"
+    
+    mkdir -p /tmp/${MODEL}/tb
+    echo "dir ${OUTDIR} before moving"
+    ls -lh ${OUTDIR}
+    mv ${OUTDIR}/*.h5 /tmp/${MODEL}/
+    mv ${OUTDIR}/tb/* /tmp/${MODEL}/tb/
+    echo "dir ${OUTDIR} after moving"
+    ls -lh ${OUTDIR}
+
+    cp -f /tmp/DQNTrainer_${PID}_*.log /tmp/${MODEL}/
+    mv -f /tmp/DQNTrainer_${PID}_*.log.*bz2 /tmp/${MODEL}/
+    
+    nice tar cvfj /tmp/${MODEL}.tar.bz2~ /tmp/${MODEL}/*
+    rm -f /tmp/${MODEL}.tar.bz2; mv -f /tmp/${MODEL}.tar.bz2~ /tmp/${MODEL}.tar.bz2
+    ls -lh /tmp/${MODEL}*.tar.bz2 ;
+fi
+
+# # keep the most recent 5 tarballs
+# STAMP=$(date +%m%d%H%M%S)
+# if [ -e /tmp/${MODEL}.tar.bz2 ] ; then 
+#     mv -f /tmp/${MODEL}.tar.bz2 /tmp/${MODEL}_${STAMP}.tar.bz2
+#     TAR2CLR=$(ls /tmp/${MODEL}_*.tar.bz2 |sort| head -n -5)
+#     echo "evicting old tarballs: ${TAR2CLR}"
+#     rm -f ${TAR2CLR} ;
+#     ls -lh /tmp/${MODEL}*.tar.bz2 ;
+# fi
+
+

a39ed6d20db064046fd1e5d2f3435cb88cd52d44
Andy Shao <hui.shao@xor-media.tv>
2020-03-12 14:21:46 +0800

    balanceSamples

--- src/launch/CsvToDQN.py
+++ src/launch/CsvToDQN.py
@@ -10,8 +10,109 @@ from Application import *
 import HistoryData as hist
 
 import sys, os, platform
+RFGROUP_PREFIX = 'ReplayFrame:'
+OUTFRM_SIZE = 8*1024
+import random
+
+def balanceSamples(filepathRFrm) :
+    '''
+    read a frame from H5 file
+    '''
+    dsargs={
+        'compression':'gzip'
+    }
+
+    print("balancing samples in %s to %sb" % (filepathRFrm, filepathRFrm))
+    with h5py.File(filepathRFrm+'b', 'w') as h5out:
+        frmId=0
+        frmState=None
+        frmAction=None
+
+        with h5py.File(filepathRFrm, 'r') as h5f:
+            framesInHd5 = []
+            for name in h5f.keys() :
+                if RFGROUP_PREFIX == name[:len(RFGROUP_PREFIX)] :
+                    framesInHd5.append(name)
+
+            framesInHd5.sort()
+
+            for frmName in framesInHd5 :
+                print("opening frm[%s]" % frmName)
+                frm = h5f[frmName]
+                if frmState is None:
+                    frmState = np.array(list(frm['state']))
+                    frmAction = np.array(list(frm['action']))
+                    lenBefore =0
+                    lenAfter = len(frmState)
+                else :
+                    lenBefore = len(frmState)
+                    a = np.array(list(frm['state']))
+                    frmState = np.concatenate((frmState, a), axis=0)
+                    a = np.array(list(frm['action']))
+                    frmAction= np.concatenate((frmAction, a), axis=0)
+                    lenAfter = len(frmState)
+
+                npActions = frmAction[lenBefore:]
+                AD = np.where(npActions >=0.99) # to match 1 because action is float read from RFrames
+                kI = [np.count_nonzero(AD[1] ==i) for i in range(3)] # counts of each actions in frame
+                cRowToKeep = max(kI[1:]) + sum(kI[1:]) # = max(kI[1:]) *3
+                idxHolds = np.where(AD[1] ==0)[0].tolist()
+                cHoldsToDel = len(idxHolds) - (cRowToKeep - sum(kI[1:]))
+                if cHoldsToDel>0 :
+                    random.shuffle(idxHolds)
+                    del idxHolds[cHoldsToDel:]
+                    idxToDel = [lenBefore +i for i in idxHolds]
+                    frmState = np.delete(frmState, idxToDel, axis=0)
+                    frmAction = np.delete(frmAction, idxToDel, axis=0)
+
+                print("frm[%s] processed, pending %s" % (frmName, len(frmState)))
+                if len(frmState) >= OUTFRM_SIZE:
+                    col_state = frmState[:OUTFRM_SIZE]
+                    col_action = frmAction[:OUTFRM_SIZE]
+                    frmState = frmState[OUTFRM_SIZE:]
+                    frmAction = frmAction[OUTFRM_SIZE:]
+
+                    g = h5out.create_group('%s%s' % (RFGROUP_PREFIX, frmId))
+                    g.create_dataset(u'title', data= 'compressed replay frame[%s]' % (frmId))
+                    g.attrs['state'] = 'state'
+                    g.attrs['action'] = 'action'
+                    g.attrs[u'default'] = 'state'
+                    g.attrs['size'] = col_state.shape[0]
+                    g.attrs['signature'] = EXPORT_SIGNATURE
+
+                    st = g.create_dataset('state', data= col_state, **dsargs)
+                    st.attrs['dim'] = col_state.shape[1]
+                    ac = g.create_dataset('action', data= col_state, **dsargs)
+                    ac.attrs['dim'] = col_action.shape[1]
+                    print("outfrm[%s] saved, pending %s" % (frmId, len(frmState)))
+                    frmId +=1
+
+            if len(frmState) >= 0:
+                col_state = frmState
+                col_action = frmAction
+                g = h5out.create_group('%s%s' % (RFGROUP_PREFIX, frmId))
+                g.create_dataset(u'title', data= 'compressed replay frame[%s]' % (frmId))
+                g.attrs['state'] = 'state'
+                g.attrs['action'] = 'action'
+                g.attrs[u'default'] = 'state'
+                g.attrs['size'] = col_state.shape[0]
+                g.attrs['signature'] = EXPORT_SIGNATURE
+
+                st = g.create_dataset('state', data= col_state, **dsargs)
+                st.attrs['dim'] = col_state.shape[1]
+                ac = g.create_dataset('action', data= col_state, **dsargs)
+                ac.attrs['dim'] = col_action.shape[1]
+
+                print("lastfrm[%s] saved, size %s" % (frmId, len(frmState)))
 
 if __name__ == '__main__':
+    if '-b' in sys.argv :
+        idx = sys.argv.index('-b') +1
+        if idx >0 and idx < len(sys.argv):
+            h5fn = sys.argv[idx]
+            balanceSamples('/mnt/e/AShareSample/500top50/RFrmD4M1X5_SH600201.h5')
+            quit()
+
     if not '-f' in sys.argv :
         sys.argv += ['-f', os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/../conf/CsvToDQN.json']
 

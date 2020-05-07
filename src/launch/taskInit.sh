0c226f502849c7b8e0cb45654af8f2018dce548e
Andy Shao <hui.shao@xor-media.tv>
2020-05-07 15:08:37 +0800

    taskOnlineSim

--- src/launch/taskInit.sh
+++ src/launch/taskInit.sh
@@ -4,6 +4,10 @@ HOSTID=$(hostname)
 echo "${HOSTID}" |grep syscheme || HOSTID=$(hostname -I)
 
 # step.1 about sync thru OneDrive
+# expected in crontab
+#   @reboot if [ -e ~/tasks/reboot ]; then ~/tasks/reboot 2>&1 > /tmp/reboot.log; fi &
+#   @reboot if [ -e ~/tasks/once ]; then rm -rf /tmp/once ; cp -vf $(realpath ~/tasks/once) /tmp/once; rm -rf ~/tasks/once; /tmp/once 2>&1 > /tmp/once.log ; fi &
+#   8 12,17,22 * * 1,2,3,4,5 /usr/bin/onedrive --synchronize 2>&1 > /tmp/oncedrive.txt &
 if ! [ -e ~/.config/onedrive/config ]; then
     echo "skip_file = \"~*|.~*|*.tmp\""      > ~/.config/onedrive/config
     echo "no_remote_delete = \"yes\""       >> ~/.config/onedrive/config
@@ -11,8 +15,12 @@ if ! [ -e ~/.config/onedrive/config ]; then
     echo "log_dir = \"/var/log/onedrive/\"" >> ~/.config/onedrive/config
 fi
 
-mkdir -p /var/log/onedrive/
 mkdir -p ~/OneDrive/deployments/${HOSTID}
+ln -s ~/OneDrive/deployments/${HOSTID} ~/deploy-data
+mkdir -p ~/deploy-data/tasks
+ln -s ~/deploy-data/tasks ~/tasks
+mkdir -p /var/log/onedrive/
+
 if ! [ -e ~/.config/onedrive/sync_list ]; then
     echo "deployments/${HOSTID}"  > ~/.config/onedrive/sync_list
 fi

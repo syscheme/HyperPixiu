12df6f30a247b7867a8abd52cb5630fe1d90df26
Andy Shao <hui.shao@xor-media.tv>
2020-05-13 11:30:47 +0800

    include trade time in txnhist

--- src/Account.py
+++ src/Account.py
@@ -937,7 +937,7 @@ class Account(MetaAccount):
                     posChange = -trade.volume
                     self._todayResult.tcSell += 1
                     
-                self._todayResult.txnHist += "%+dx%s@%s" % (posChange, trade.symbol, formatNumber(trade.price,3))
+                self._todayResult.txnHist += "%+dx%s@%sP%s" % (posChange, trade.symbol, trade.asof.strftime('%H%M'), formatNumber(trade.price, 3))
 
                 self._todayResult.tradingPnl += round(posChange * (self._marketState.latestPrice(trade.symbol) - trade.price) * self._contractSize, 2)
                 turnover, commission, slippagefee = self.calcAmountOfTrade(trade.symbol, trade.price, trade.volume)

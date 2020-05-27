575dddac8f5b27a463f76f00441d69b7eb31449d
Andy Shao <hui.shao@xor-media.tv>
2020-05-27 17:15:22 +0800

    allow repeatly exec taskAdvisor.sh

--- src/launch/taskAdvisor.sh
+++ src/launch/taskAdvisor.sh
@@ -6,13 +6,19 @@ BAKSTAMP=$(date +%Y%m%dT%H%M%S)
 
 cd ${TOPDIR_HP}
 OUTDIR=./out/advisor
-CONFFILE=./conf/Advisor.json
+CONF=./conf/Advisor.json
+
+PID=$(ps aux|grep 'advisor.py'|grep ${CONF}|awk '{print $2;}')
+if ! [ -z ${PID} ]; then
+    echo "an existing advisor is running with PID=${PID}"
+    exit 0
+fi
 
 PID_LIST=$(ls ${OUTDIR}/*.tcsv |sed 's/^.*advisor_\([0-9]*\).*tcsv/\1/g')
 for i in ${PID_LIST}; do
-    mv -vf /tmp/advisor_${i}_*.log ${OUTDIR}/ ; 
+    mv -vf /tmp/advisor_${i}_*.log* ${OUTDIR}/ ; 
 done
-cp -vf ${CONFFILE} ${OUTDIR}/
+cp -vf ${CONF} ${OUTDIR}/
 
 mv ${OUTDIR} ${OUTDIR}.BAK${BAKSTAMP}
 mkdir -p ${OUTDIR}
@@ -27,6 +33,13 @@ OBJ_LIST="${OBJ_LIST}]"
 echo ${OBJ_LIST}
 
 SED_STATEMENT="s/^[ \t]*\\\"objectives\\\".*:.*/   \\\"objectives\\\": ${OBJ_LIST}, \/\/ updated at ${BAKSTAMP}/g"
-sed -i "${SED_STATEMENT}" ${CONFFILE}
+sed -i "${SED_STATEMENT}" ${CONF}
 
-./run.sh ./src/launch/advisor.py -f ${CONFFILE} &
+SED_STATEMENT="s/^.*\\\"console\\\".*:.*/   \\\"console\\\": \\\"False\\\", \/\/ updated at ${BAKSTAMP}/g"
+sed -i "${SED_STATEMENT}" ${CONF}
+
+./run.sh ./src/launch/advisor.py -f ${CONF} 2>&1 >/dev/null &
+
+sleep 1 # to wait the above command starts
+PID=$(ps aux|grep 'advisor.py'|grep ${CONF}|awk '{print $2;}')
+echo "started advisor with PID=${PID}"

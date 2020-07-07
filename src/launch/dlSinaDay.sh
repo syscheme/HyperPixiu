d4102196cdfe15468c45bcc0794110d8193abb1a
Andy Shao <hui.shao@xor-media.tv>
2020-07-07 18:45:02 +0800

    dlSinaDay covers ETFs

--- src/launch/dlSinaDay.sh
+++ src/launch/dlSinaDay.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+CMD=${0##*/}
 SRC_DIR=$(realpath `dirname $0`)
 
-SYMBOLLIST=$(bzcat ${SRC_DIR}/symbols.txt.bz2)
+SYMBOLLIST=$(bzcat ${SRC_DIR}/symbols.txt.bz2|grep -o '^[Ss].[0-9]*' | tr '[:lower:]' '[:upper:]')
 # SYMBOLLIST="SZ002881 SH600996 SZ002230"
 DATE=$(date +%Y%m%d)
 TARGETDIR="$(realpath ~/hpdata)"
+DATALEN=9999
 
 #-------------------------------------
 RET=200
@@ -20,8 +21,14 @@ downloadMF1m()
             grep -o netamount ${FN} >/dev/null && echo "MF1m of ${SYMBOL} has already been downloaded" && return
     fi
 
+    PREFIX=$(echo $SYMBOL | cut -c 1-4)
+    if [ "SH51" == "${PREFIX}" ] || [ "SZ15" == "${PREFIX}" ]; then
+        RET=200
+        return
+    fi
+
     echo "fetching MF1m of ${SYMBOL} to ${FN}"
-    RET=$(wget --user-agent="${UA}" "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/MoneyFlow.ssx_ggzj_fszs?sort=time&num=260&page=1&daima=${SYMBOL}" -O ${FN} 2>&1|grep -o 'awaiting response.*'| grep -o '[0-9]*')
+    RET=$(wget --user-agent="${UA}" "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/MoneyFlow.ssx_ggzj_fszs?sort=time&num=${DATALEN}&page=1&daima=${SYMBOL}" -O ${FN} 2>&1|grep -o 'awaiting response.*'| grep -o '[0-9]*')
     if [ "200" == "${RET}" ]; then
         echo "downloaded MF1m of ${SYMBOL} as ${FN}, resp ${RET}"
         return
@@ -31,17 +38,38 @@ downloadMF1m()
     rm -f ${FN}
 }
 
-downloadKL5m()
+downloadMF1d()
 {
     SYMBOL=$1
     FN=$2
+
+    if [ -e ${FN} ]; then
+            grep -o netamount ${FN} >/dev/null && echo "MF1m of ${SYMBOL} has already been downloaded" && return
+    fi
+
+    echo "fetching MF1m of ${SYMBOL} to ${FN}"
+    RET=$(wget --user-agent="${UA}" "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/MoneyFlow.ssl_qsfx_zjlrqs?daima=${SYMBOL}" -O ${FN} 2>&1|grep -o 'awaiting response.*'| grep -o '[0-9]*')
+    if [ "200" == "${RET}" ]; then
+        echo "downloaded MF1m of ${SYMBOL} as ${FN}, resp ${RET}"
+        return
+    fi
+    
+    echo "failed to download MF1d of ${SYMBOL}, resp ${RET}"
+    rm -f ${FN}
+}
+
+downloadKLXm()
+{
+    SYMBOL=$1
+    FN=$2
+    SCALE=$3
 
     if [ -e ${FN} ]; then
             grep -o volume ${FN} >/dev/null && echo "KL5m of ${SYMBOL} has already been downloaded" && return
     fi
 
     echo "fetching KL5m of ${SYMBOL} to ${FN}"
-    RET=$(wget --user-agent="${UA}" "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=${SYMBOL}&scale=5&datalen=100" -O ${FN} 2>&1|grep -o 'awaiting response.*'| grep -o '[0-9]*')
+    RET=$(wget --user-agent="${UA}" "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=${SYMBOL}&scale=${SCALE}&datalen=${DATALEN}" -O ${FN} 2>&1|grep -o 'awaiting response.*'| grep -o '[0-9]*')
     if [ "200" == "${RET}" ]; then
         echo "downloaded KL5m of ${SYMBOL} as ${FN}, resp ${RET}"
         return
@@ -49,6 +77,16 @@ downloadKL5m()
     
     echo "failed to download KL5m of ${SYMBOL}, resp ${RET}"
     rm -f ${FN}
+}
+
+downloadKL5m()
+{
+    downloadKLXm "$1" "$2" 5
+}
+
+downloadKL1d()
+{
+    downloadKLXm "$1" "$2" 240
 }
 
 downloadList()
@@ -106,7 +144,47 @@ downloadList()
     fi
 }
 
+echo "$(date) $0 starts"
 cd /tmp
 
-downloadList downloadMF1m
-downloadList downloadKL5m
+case ${CMD} in
+        MF1m)
+        if [ "*" == "$1" ] || [ -z "$1" ]; then
+            downloadList downloadMF1m
+        else
+            downloadMF1m $1 ${1}_${CMD}${DATE}.json
+        fi
+                ;;
+
+        MF1d)
+        if [ "*" == "$1" ] || [ -z "$1" ]; then
+            downloadList downloadMF1d
+        else
+            downloadMF1d $1 ${1}_${CMD}${DATE}.json
+        fi
+                ;;
+
+        KL5m)
+        if [ "*" == "$1" ] || [ -z "$1" ]; then
+            downloadList downloadKL5m
+        else
+            downloadKL5m $1 ${1}_${CMD}${DATE}.json
+        fi
+                ;;
+
+        KL1d)
+        if [ "*" == "$1" ] || [ -z "$1" ]; then
+            downloadList downloadKL1d
+        else
+            downloadKL1d $1 ${1}_${CMD}${DATE}.json
+        fi
+                ;;
+
+    *)
+        DATALEN=300
+        downloadList downloadMF1m
+        DATALEN=100
+        downloadList downloadKL5m
+                ;;
+
+esac

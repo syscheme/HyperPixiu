c75db648f8016087bfae5aa5959dd99e480c174b
root <root@dazzle.localdomain>
2020-10-18 09:05:37 +0800

    h5tar support precompressed and show

--- src/launch/h5tar.py
+++ src/launch/h5tar.py
@@ -14,6 +14,8 @@ GNAME_TEXT_utf8 = 'utf8_bz2'
 
 def tar_utf8(fn_h5tar, fn_members, createmode='a') :
 
+    createmode = createmode if 'a'==createmode else 'w'
+
     with h5py.File(fn_h5tar, createmode) as h5file:
         g = h5file.create_group(GNAME_TEXT_utf8) if not GNAME_TEXT_utf8 in h5file.keys() else h5file[GNAME_TEXT_utf8]
         g.attrs['desc']         = 'text member files via utf-8 encoding and bzip2 compression'
@@ -23,17 +25,24 @@ def tar_utf8(fn_h5tar, fn_members, createmode='a') :
             except:
                 continue
 
+            print('adding %s\tsize:%s' % (m, filesize))
+            if '.bz2' == m[-4:]:
+                with open(m, 'rb') as mf:
+                    compressed = mf.read()
+                m = m[:-4]
+                filesize = '%sz' % filesize
+            else :
+                with open(m, 'r') as mf:
+                    all_lines = mf.read()
+                    compressed = bz2.compress(all_lines.encode('utf8'))
+
+            npbytes = np.frombuffer(compressed, dtype=np.uint8)
             k = quote(m).replace('/','%2F')
             if k in g.keys():
                 del g[k]
-                
-            print('adding %s\tsize:%s' % (m, filesize))
-            with open(m, 'r') as mf:
-                all_lines = mf.read()
-                compressed = bz2.compress(all_lines.encode('utf8'))
-                npbytes = np.frombuffer(compressed, dtype=np.uint8)
-                sub = g.create_dataset(k, data=npbytes)
-                sub.attrs['size'] = filesize
+            sub = g.create_dataset(k, data=npbytes)
+            sub.attrs['size'] = filesize
+            sub.attrs['csize'] = len(compressed)
 
 def untar_utf8(fn_h5tar, fn_members =None) :
 
@@ -66,21 +75,43 @@ def untar_utf8(fn_h5tar, fn_members =None) :
 
 def list_utf8(fn_h5tar, fn_members =None) :
 
+    ret = []
+
     with h5py.File(fn_h5tar, 'r') as h5file:
         if not GNAME_TEXT_utf8 in h5file.keys() :
             print('no group[utf8_bz2] in %s', fn_h5tar)
             return
 
         g = h5file[GNAME_TEXT_utf8]
-        print('members:\n\t%s' % '\n\t'.join([ '%s\t%s' % (unquote(i), g[i].attrs['size']) for i in g.keys()]))
+        for i in g.keys():
+            ret.append({'name':unquote(i), 'csize': g[i].attrs['csize'], 'size': g[i].attrs['size']})
+        return ret
+
+def read_utf8(fn_h5tar, fn_member) :
+    with h5py.File(fn_h5tar, 'r') as h5file:
+        if not GNAME_TEXT_utf8 in h5file.keys() :
+            print('no group[utf8_bz2] in %s', fn_h5tar)
+            return
+
+        g = h5file[GNAME_TEXT_utf8]
+        m = quote(fn_member).replace('/','%2F')
+        if not m in g.keys():
+            print('member[%s] not found' % fn_members)
+            return ''
+        
+        compressed = g[m].value.tobytes()
+        all_lines = bz2.decompress(compressed).decode('utf8')
+        return all_lines
+
+    return ''
 
 ########################################################################
 if __name__ == '__main__':
 
-    # tar_utf8('abc.h5', ['/tmp/SinaWeek.20200615/SinaKL5m_20200615/SZ399997_KL5m20200615.json'])
+    # tar_utf8('abc.h5t', ['SZ399997_KL5m20200615.json.bz2'])
 
     if len(sys.argv) <3:
-        print('%s {list|s|create|c|extract|x|add|a} <tarfilename> [textfile1 [textfile2 ...]]' % os.path.basename(sys.argv[0]))
+        print('%s {list|s|create|c|extract|x|add|a|show|s} <tarfilename> [textfile1 [textfile2 ...]]' % os.path.basename(sys.argv[0]))
         exit(0)
 
     cmd = sys.argv[1]
@@ -88,9 +119,12 @@ if __name__ == '__main__':
     fn_members= sys.argv[3:]
 
     if 'l' == cmd[0]:
-        list_utf8(fn_h5tar)
+        lst = list_utf8(fn_h5tar)
+        print('members:\n\t%s' % '\n\t'.join([ '%s\t%s/%s' % (i['name'], i['csize'], i['size']) for i in lst]) )
     elif ('c' == cmd[0] or 'a' == cmd[0]) and len(fn_members) >0:
         tar_utf8(fn_h5tar, fn_members, cmd[0])
     elif 'x' in cmd:
         untar_utf8(fn_h5tar, fn_members)
+    elif 's' in cmd and len(fn_members) >0:
+        print(read_utf8(fn_h5tar, fn_members[0]))
 

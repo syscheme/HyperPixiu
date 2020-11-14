6cc76062612e4715f400772e7eae4669d70b7486
root <root@AHS-X1Y.localdomain>
2020-11-14 22:14:56 +0800

    subdir distributed

--- src/distributed/addworker.sh
+++ src/distributed/addworker.sh
@@ -2,11 +2,11 @@ WORKER=$1
 # the master was created like below
 # useradd -d /mnt/data/hpwkspace hpx # the master
 # cd /mnt/data/; chown -R hpx:hpx hpwkspace
-# [root@tc2 hpwkspace]# ls -l /mnt/data/hpwkspace
+# # ls -l /mnt/data/hpwkspace
 # drwxr-xr-x 2 hpx hpx 16384 Nov 12 12:58 archived
 # drwxr-xr-x 3 hpx hpx    22 Nov 12 14:18 master
 # drwxrwxr-x 4 hpx hpx    29 Nov 14 14:04 workers
-# [root@tc2 hpwkspace]# grep hpx /etc/group
+# # grep hpx /etc/group
 # hpx:x:1000:root
 
 useradd -b /mnt/data/hpwkspace/workers -g hpx ${WORKER}
@@ -19,7 +19,7 @@ sudo -u ${WORKER} bash -c "cd; echo | ssh-keygen -q -t rsa -N ''; tar xfvj ../..
 # tar cfvj ../../master/wkr_prof_template.tar.bz2 .bashrc .ssh archived hpx_template
 
 # the local of a worker, assuming /mnt/s is sshfs-mounted to the ${WORKER}@master-host
-# [root@AHS-X1Y wkspaces]# ls -l ~/wkspaces/*
+# # ls -l ~/wkspaces/*
 # lrwxrwxrwx 1 root root 15 Nov 14 15:10 hpx_archived -> /mnt/s/archived
 # lrwxrwxrwx 1 root root 17 Nov 14 15:16 hpx_publish -> /mnt/s/to_publish
 # -rw-rw-rw- 1 root root 204 Nov 14 15:31 hpx_rsync_excl.txt

1346908d912fa43e97cb7892871eec51143618f9
Andy Shao <hui.shao@xor-media.tv>
2021-02-08 18:23:51 +0800

    new DnnAdvisor draft

--- src/advisors/dnn.py
+++ src/advisors/dnn.py
@@ -177,3 +177,111 @@ class DnnAdvisor_S1548I4A3(TradeAdvisor):
     # end of BaseApplication routine
     #----------------------------------------------------------------------
 
+########################################################################
+class DnnAdvisor(TradeAdvisor):
+    '''
+    DnnAdvisor impls TradeAdvisor by employing a pre-trained DNN model for state S1548I4A3
+    '''
+    STATE_DIMS  = 1548
+    ITEM_FLOATS = EXPORT_FLOATS_DIMS
+    ACTION_DIMS = len(ADVICE_DIRECTIONS) # =3
+
+    def __init__(self, program, **kwargs) :
+        self._brainId = None
+        super(DnnAdvisor, self).__init__(program, **kwargs)
+
+        self._fnModel        = self.getConfig('model', None)
+        self._stateFormatId  = self.getConfig('stateFormat', "DDDD")
+        self._processor = self.getConfig('processor', None)
+        self._brain   = None
+        self.__id     = None
+
+    @property
+    def ident(self) :
+        if not self.__id:
+            if self._brainId : id = self._brain.modelId
+            else:
+                id = os.path.basename(self._fnModel)
+                if '.h5' == id[-3:] : id = id[:-3]
+                if '.json' == id[-5:] : id = id[:-5]
+
+            self.__id = 'dnn.%s' % id
+
+        return self.__id
+
+    def generateAdviceOnMarketEvent(self, ev, lastAdv=None):
+        '''processing an incoming MarketEvent and generate an advice
+            @param lastAdv is a reference in the case the advisor wish to refer to 
+        '''
+
+        if MARKETDATE_EVENT_PREFIX != ev.type[:len(MARKETDATE_EVENT_PREFIX)] :
+            self.debug('generateAdviceOnMarketEvent() ignored event %s' % ev.type)
+            return None
+
+        if not EVENT_KLINE_PREFIX in ev.type and not EVENT_TICK in ev.type :
+            self.debug('generateAdviceOnMarketEvent() not yet support event %s' % ev.type)
+            return None
+
+        d = ev.data
+        tokens = (d.vtSymbol.split('.'))
+        symbol = tokens[0]
+
+        # because the gain-size of S1548I4A3 is KL1m, it is unnecessary to perform perdiction more freq-ly than once-per-minute
+        if lastAdv and d.asof.minute == lastAdv.asof.minute:
+            secAgo = datetime2float(d.asof) - datetime2float(lastAdv.asof)
+            if (EVENT_TICK == ev.type and d.asof.second <5.0) or secAgo < 30.0 : # maximal twice during a same minute
+                self.debug('generateAdviceOnMarketEvent() recently adviced %ss ago, skip predicting on event: %s' % (secAgo, ev.desc))
+                return None
+
+        # floatstate = self._marketState.exportF1548(symbol)
+        fmtr = Formatter_F1548()
+        floatstate = self._marketState.format(fmtr, symbol)
+        if all(v == 0.0 for v in floatstate):
+            self.debug('generateAdviceOnMarketEvent() rack of marketState on %s' % ev.desc)
+            return None # skip advising pirior to plenty state data
+
+        floatstate = np.array(floatstate).astype(NN_FLOAT).reshape(1, DnnAdvisor_S1548I4A3.STATE_DIMS)
+        act_values = self._brain.predict(floatstate)
+        # action = [0.0] * DnnAdvisor_S1548I4A3.ACTION_DIMS
+        # idxAct = np.argmax(act_values[0])
+        # action[idxAct] = 1.0
+        advice = AdviceData(self.ident, symbol, d.exchange)
+        advice.dirNONE, advice.dirLONG, advice.dirSHORT = act_values[0][0], act_values[0][1], act_values[0][2]
+        advice.price = d.close if EVENT_KLINE_PREFIX == ev.type[:len(EVENT_KLINE_PREFIX)] else d.price
+
+        return advice
+
+    #----------------------------------------------------------------------
+    # impl/overwrite of BaseApplication
+    def doAppInit(self): # return True if succ
+
+        brainDir = '%s%s.S1548I4A3/' % (self.dataRoot, self._brainId)
+        if self._processor and len(self._processor) >1: # if specified to run on a given processor
+            from tensorflow.python.client import device_lib
+            local_device_protos = device_lib.list_local_devices()
+            devname = None
+            if '/device:' in self._processor:
+                devname = self._processor
+            else:
+                self._processor = self._processor.upper()
+                for x in local_device_protos:
+                    if x.device_type == self._processor or self._processor == x.name[len('/device:'):]:
+                        devname = x.name
+                        break
+            if devname:
+                devname = devname[len('/device:'):]
+                with tf.device('/%s' % devname) :
+                    self._brain = _loadBrain(self, brainDir)
+
+        if not self._brain:
+            self._brain = _loadBrain(self, brainDir)
+
+        if not self._brain:
+            self.error('doAppInit() failed to load brain[%s]' %self._brainId)
+            return False
+
+        return super(DnnAdvisor_S1548I4A3, self).doAppInit()
+
+    # end of BaseApplication routine
+    #----------------------------------------------------------------------
+
